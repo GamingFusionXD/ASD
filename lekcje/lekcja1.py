3e3589655a58fc646bf4248a7a122ec610f7b1ca
GamingFusionXD <97955742+GamingFusionXD@users.noreply.github.com>
2022-04-20 12:24:14 +0200

    Add files via upload

---
+++ lekcje/lekcja1.py
@@ -0,0 +1,93 @@
+import pygame
+import random
+import waz
+import jablko
+rozdzielczosc=500
+obiektWaz1=waz.Waz()
+obiektWaz2=waz.Waz()
+def zmienKolorWaz1(kolor):
+    obiektWaz1.setColor(kolor)
+def zmienKolorWaz2(kolor):
+    obiektWaz2.setColor(kolor)
+
+iloscJablek=5
+def main():
+    
+    obiektJablko=[]
+    for nrJablka in range(0,iloscJablek):
+        obiektJablko.append(jablko.Jablko())
+
+    
+    pygame.init()
+    Oknogry=pygame.display.set_mode((rozdzielczosc,rozdzielczosc),0,32)
+    run=True
+
+    
+    while(run):
+        glowa=obiektWaz1.getHeadPosition()
+        glowaWaz1X=glowa[0]
+        glowaWaz1Y=glowa[1]
+        glowa2=obiektWaz2.getHeadPosition()
+        glowaWaz2X=glowa2[0]
+        glowaWaz2Y=glowa2[1]
+        Oknogry.fill((0,0,0))
+        pygame.time.delay(100)
+        
+        
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            #sterowanie weża
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    obiektWaz1.setDirection((-1,0))
+                elif event.key == pygame.K_RIGHT:
+                    obiektWaz1.setDirection((1,0))
+                elif event.key == pygame.K_UP:
+                    obiektWaz1.setDirection((0,-1))
+                elif event.key == pygame.K_DOWN:
+                    obiektWaz1.setDirection((0,1))
+                elif event.key == pygame.K_a:
+                    obiektWaz2.setDirection((-1,0))
+                elif event.key == pygame.K_d:
+                    obiektWaz2.setDirection((1,0))
+                elif event.key == pygame.K_w:
+                    obiektWaz2.setDirection((0,-1))
+                elif event.key == pygame.K_s:
+                    obiektWaz2.setDirection((0,1))
+                #sprawdzanie czy waz nie zjada siebie
+                
+        obiektWaz1.snakeMove()
+        obiektWaz2.snakeMove()        
+        #rysowanie węża
+        obiektWaz1.drawSnake(Oknogry)
+        obiektWaz2.drawSnake(Oknogry)    
+            #zjedzenie jablka
+        #rysowanie jablka
+        for nrJablka in obiektJablko[::]:
+                
+            pozycjaJablka=nrJablka.getPosition()
+            if glowaWaz1X==pozycjaJablka[0]-10 and glowaWaz1Y==pozycjaJablka[1]-10:
+                obiektWaz1.snakeEat()
+                
+                #dlugosc=dlugosc+1
+                #losowanie pozycji jablka
+                nrJablka.randPosition()
+            if glowaWaz2X==pozycjaJablka[0]-10 and glowaWaz2Y==pozycjaJablka[1]-10:
+                obiektWaz2.snakeEat()
+                
+                #dlugosc=dlugosc+1
+                #losowanie pozycji jablka
+                nrJablka.randPosition()
+            #rysowanie jabłka
+            nrJablka.drawApple(Oknogry)
+        obiektWaz1.bitMe(glowa2)
+        obiektWaz2.bitMe(glowa)
+        czcionka=pygame.font.SysFont('comicsans',20)
+        tekst=czcionka.render("Punkty {0}".format(obiektWaz1.punkty),1,(255,255,0))
+        tekst2=czcionka.render("Punkty {0}".format(obiektWaz2.punkty),1,(255,255,0))
+        Oknogry.blit(tekst,(10,10))
+        Oknogry.blit(tekst2,(10,30))
+       
+        pygame.display.update()
---
+++ lekcje/waz.py
@@ -0,0 +1,77 @@
+import pygame
+import lekcja1
+class Waz():
+    #konstruktor klasy - uruchamia się podczas utworzenia obiektu
+    def __init__(self):
+        self.dlugosc=1
+        self.pozycja=[(160,160)]
+        self.punkty=0
+        self.kierunek=(0,1)
+        self.kolor=(255,0,0)
+    #ustawia kolor węża
+    def setColor(self,nowyKolor):
+        self.kolor=nowyKolor
+    #ustalenie kierunku węża
+    def setDirection(self,direction):
+        self.kierunek=direction
+    #zwrócenie pozycji głowy
+    def getHeadPosition(self):
+        return self.pozycja[-1]
+    #dodanie nowej pozycj węża
+    def addPosition(self,x,y):
+        self.pozycja.append((x,y))
+    #wykonanie ruchu przez węża
+    def snakeMove(self):
+        #generowanie nowych pozycji
+        #ostatnia pozycja glowy
+        ostatniaPozycja=self.pozycja[-1]
+        x=ostatniaPozycja[0]+20*self.kierunek[0]
+        y=ostatniaPozycja[1]+20*self.kierunek[1]
+        #sprawdzanie krawedzi
+        noweWspolrzedne=self.checkBorder(x,y)
+        #sprawdzenie czy wąż nie zjadł sam siebie
+        for czesciCiala in self.pozycja[::]:
+            if czesciCiala[0]==noweWspolrzedne[0] and czesciCiala[1]==noweWspolrzedne[1]:
+                self.dlugosc=1
+                self.punkty=0
+            
+        #dodanie nowej pozycji
+        if self.dlugosc==1:
+            self.pozycja=[noweWspolrzedne]
+        else:
+            self.addPosition(noweWspolrzedne[0],noweWspolrzedne[1])
+        #jeżeli wąż jest za dlugi, usunięcie pierwszej pozycji
+        if len(self.pozycja)>self.dlugosc:
+            del self.pozycja[0]
+    #zwiększanie paramtrów w momencie zjadania jablka
+    def snakeEat(self):
+        #self.dlugosc=self.dlugosc+1
+        self.dlugosc+=1
+        self.punkty+=1
+    #rysowanie węża
+    def drawSnake(self, Oknogry):
+        for position in self.pozycja[::-1]:
+            kwadrat = pygame.Rect((position[0],position[1]),(20,20))
+            pygame.draw.rect(Oknogry,self.kolor,kwadrat)
+    #sprawdzanie krawedzi okna
+    def checkBorder(self,zmienna1,zmienna2):
+        #przejscie prawa
+        if zmienna1>lekcja1.rozdzielczosc:
+            zmienna1=0
+        #przejscie dol
+        if zmienna2>lekcja1.rozdzielczosc:
+            zmienna2=0
+        #przejscie lewa
+        if zmienna1<0:
+            zmienna1=lekcja1.rozdzielczosc
+        #przejscie gora
+        if zmienna2<0:
+            zmienna2=lekcja1.rozdzielczosc
+        return (zmienna1,zmienna2)
+    #sprawdzanie czy ktoś mnie nie zjadł
+    def bitMe(self,pozycje):
+        for czesciCiala in self.pozycja[::]:
+            if pozycje[0]==czesciCiala[0] and pozycje[1] ==czesciCiala[1]:
+                self.pozycja=[(pozycje[0],pozycje[1])]
+                self.dlugosc=1
+                self.punkty=0
