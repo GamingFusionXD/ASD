038f90ed1f11d5a60480c9e6eb4424fe88972763
GamingFusionXD <97955742+GamingFusionXD@users.noreply.github.com>
2023-02-01 16:33:53 +0100

    Add files via upload

--- formularz0102/index.php
+++ formularz0102/index.php
@@ -17,6 +17,18 @@
         <label for="pesel">Podaj pesel</label>
         <input type="text" name="pesel" id="pesel">  
         </br>
+        <label for="kod_pocztowy">Podaj kod pocztowy</label>
+        <input type="text" name="kod_pocztowy" id="kod_pocztowy">  
+        </br>
+        <label for="miejscowosc">Podaj miejscowosc</label>
+        <input type="text" name="miejscowosc" id="miejscowosc">  
+        </br>
+        <label for="ulica">Podaj ulice</label>
+        <input type="text" name="ulica" id="ulica">  
+        </br>
+        <label for="numer">Podaj numer</label>
+        <input type="text" name="numer" id="numer">  
+        </br>
         <input type="submit" value="zapisz">
     </form> 
     <?php
@@ -26,16 +38,24 @@
             $imie=$_POST['imie'];
             $nazwisko=$_POST['nazwisko'];
             $pesel=$_POST['pesel'];
-            $insert="insert into dane values(null,'$imie','$nazwisko','$pesel')";
+            $kodpocztowy=$_POST['kod_pocztowy'];
+            $miejscowosc=$_POST['miejscowosc'];
+            $ulica=$_POST['ulica'];
+            $numer=$_POST['numer'];
+            if(empty($pesel)){
+                echo "Dopisz pesel";
+            }
+        else{
+            $insert="insert into dane values(null,'$imie','$nazwisko','$pesel','$kodpocztowy','$miejscowosc','$ulica','$numer')";
             if(mysqli_query($polaczenie,$insert)){
-            echo "dodano do bazy";
-            
-       }
-       else{
-            echo "Błąd";
-       }
+                echo "dodano do bazy";
+            }
+            else{
+                    echo "Błąd";
+            }
+        }
     }
-}
+    }
     ?>
 </body>
 </html>
