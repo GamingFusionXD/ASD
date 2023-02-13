e5ef6aef36b4b80e50a4de2a81503832e55527ed
GamingFusionXD <97955742+GamingFusionXD@users.noreply.github.com>
2023-02-13 12:10:53 +0100

    Add files via upload

--- asda/index.php
+++ asda/index.php
@@ -12,21 +12,35 @@
             Dodawanie obecności
         </h1>
     </header>
-    <?php
-        $polaczenie=mysqli_connect('localhost','root','','4tie');
-        if($polaczenie){
-            echo "Połączyliśmy się z bazą danych";
-            $zapytanie="SELECT * FROM dane";
-            $wynik=mysqli_query($polaczenie,$zapytanie);
-            while($wiersz=mysqli_fetch_array($wynik)){
-                $imie[]= $wiersz['imie'];
-                $nazwisko[]= $wiersz['nazwisko'];
-                $iddane[]= $wiersz['id'];
+    <section>
+        <?php
+            $polaczenie=mysqli_connect('localhost','root','','4tie');
+            if($polaczenie){
+                echo "Połączyliśmy się z bazą danych";
+                $zapytanie="SELECT * FROM dane";
+                $wynik=mysqli_query($polaczenie,$zapytanie);
+                while($wiersz=mysqli_fetch_array($wynik)){
+                    $imie[]= $wiersz['imie'];
+                    $nazwisko[]= $wiersz['nazwisko'];
+                    $iddane[]= $wiersz['id'];
+                }
             }
-        }
-    ?>
-    <h2>
-        Lista obecności
-    </h2>
+        ?>
+        <h2>
+            Lista obecności
+        </h2>
+        <form method="POST">
+            <ol>
+            <?php
+                for ($x=0;$x<count($imie);$x++){
+                    echo "
+                    <li>
+                        <input type='checkbox' value=$iddane[$x] id='osoba$iddane[$x]' name='osoba$iddane[$x]'>
+                        <label for='osoba$iddane[$x]'>$imie[$x] $nazwisko[$x]</label>
+                        </li>
+                        ";
+                }
+            ?>
+    <section>
 </body>
 </html>
