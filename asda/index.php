f24452cd424d328c9cdc8000ae1b9753fc39c01b
GamingFusionXD <97955742+GamingFusionXD@users.noreply.github.com>
2023-02-24 14:24:47 +0100

    Update index.php

--- asda/index.php
+++ asda/index.php
@@ -4,43 +4,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>zadanie</title>
+    <title>Wiosna</title>
 </head>
 <body>
-    <header>
-        <h1>
-            Dodawanie obecności
-        </h1>
-    </header>
-    <section>
-        <?php
-            $polaczenie=mysqli_connect('localhost','root','','4tie');
-            if($polaczenie){
-                echo "Połączyliśmy się z bazą danych";
-                $zapytanie="SELECT * FROM dane";
-                $wynik=mysqli_query($polaczenie,$zapytanie);
-                while($wiersz=mysqli_fetch_array($wynik)){
-                    $imie[]= $wiersz['imie'];
-                    $nazwisko[]= $wiersz['nazwisko'];
-                    $iddane[]= $wiersz['id'];
-                }
-            }
-        ?>
-        <h2>
-            Lista obecności
-        </h2>
-        <form method="POST">
+    <a name="góra"></a>
+
+    <section id="nagłówek">
+        <h1>Witaj wiosno</h1>
+        <p>Sebastian</p>
+        <a name="poczatek"></a>
+
+    </section>
+    <section id="nav">
+        <a href="https://onet.pl"><input type="button" value="onet.pl"></a>
+        <p>"Witaj w <b><u>nowym świecie</u></b> - pełno w nim H<sub>2</sub>O"</p>
+        <a href="http://127.0.0.1:3000/index.html"><label id="odswiezenie">Odświeżenie</label></a>
+       </section>
+        <section id="główna">
+            <h2>Wszystko się budzi</h2>
+            <p>opowiedz o swoich doświadczeniach</p>
             <ol>
-            <?php
-                for ($x=0;$x<count($imie);$x++){
-                    echo "
-                    <li>
-                        <input type='checkbox' value=$iddane[$x] id='osoba$iddane[$x]' name='osoba$iddane[$x]'>
-                        <label for='osoba$iddane[$x]'>$imie[$x] $nazwisko[$x]</label>
-                        </li>
-                        ";
-                }
-            ?>
-    <section>
+                <li>w lesie</li>
+                <li>nad morzem</li>
+                <li>nad jeziorem</li>      
+            </ol>
+            <img src="img.jpg" width="150px" height="100px">
+            <figcaption>plaża</figcaption>
+            <a href="#poczatek">Początek</a>
+            </section>
+            <section id="form">
+                <form method="get">
+                    <label>Imię:</label>
+                    <input type="text"><br>
+                    <label>hasło:</label>
+                    <input type="password"><br>
+                    <label>Email: </label>
+                    <input type="email"><br>
+                    <input type="submit" value="zatwierdź">
+
+                </form>
+            </section>
+            <section id="stopka">
+                <h3>Dziękuje za wejście na stronę</h3>
+                <a href="#góra">Góra strony</a>
+                <p>"O 12<sup>00</sup> słońce jest najwyżej na <s>horyzoncie</s> <u>Załóż wtedy czapkę z daszkiem!</u></p>
+            </section>
+
+        
+   
 </body>
 </html>
