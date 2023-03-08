35c01c19dbdf17a078ecc14df2ef77bf3ed7aca2
GamingFusionXD <97955742+GamingFusionXD@users.noreply.github.com>
2023-03-08 14:41:11 +0100

    Update index.php

--- asda/index.php
+++ asda/index.php
@@ -1,4 +1,1 @@
-1. SELECT czas, kierunek, nr_rejsu, status_lotu from przyloty order by czas desc; 
-2. SELECT COUNT(*) FROM `przyloty` WHERE czas BETWEEN '10:00' and '10:59'; 
-3. SELECT przyloty.nr_rejsu, samoloty.typ from przyloty, samoloty where samoloty.id=przyloty.samoloty_id and przyloty.kierunek like "Warszawa";
-4. UPDATE `odloty` SET `status_lotu` = 'opznienie 20 min' WHERE `odloty`.`id` = 5; 
+
