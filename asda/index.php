7ea65e3d99581dd30e6be8f39271ed30fb00c363
GamingFusionXD <97955742+GamingFusionXD@users.noreply.github.com>
2023-05-23 13:45:44 +0200

    Update index.php

--- asda/index.php
+++ asda/index.php
@@ -1,181 +1,95 @@
-$<!DOCTYPE html>
-<html>
+<!DOCTYPE html>
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <title>Szkoła ponadgimnazjalna</title>
-    
-    <style>
-       
-        body {
-            font-family: Tahoma, Arial, sans-serif;
-        }
-        .banner {
-            background-color: #af8c4b;
-            
-            text-align: center;
-            height: 60px;
-        }
-        .left-panel {
-            float: left;
-            width: 30%;
-            
-            background-color: #af8c4b;
-            height: 500px;
-            text-align: right;
-        }
-        .right-panel {
-            float: right;
-            width: 70%;
-            
-            background-color: #ffd87e;
-            height: 500px;
-            overflow-y: scroll;
-        }
-        .footer {
-            clear: both;
-            text-align: center;
-            
-            background-color: #af8c4b;
-            height: 25px;
-        }
-        .horizontal-line {
-            border-top: 1px solid #ffd87e;
-            margin: 20px 0;
-        }
-        h1, h3, h4 {
-            margin-top: 0;
-        }
-        a {
-            color: #ffd87e;
-        }
-        ul {
-            list-style-type: circle;
-        }
-        table {
-            border-collapse: collapse;
-            border: 1px dotted black;
-            width: 100%;
-        }
-        th, td {
-            border: 1px dotted black;
-            padding: 5px;
-        }
-    </style>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sprawdzian JavaScript - zadania egzaminacyjne</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
-    <section class="banner">
-        <h1>Projekt strony internetowej szkoły ponadgimnazjalnej</h1>
+    <section class="baner">
+        <h1>Strona z zadaniami z JavaScript</h1>
     </section>
-
-    <section class="left-panel">
-        <h3>Do pobrania</h3>
-        <ul>
-            <li><a href="baza/zapytania.txt">Zapytania SQL</a></li>
-            <li><a href="baza/szkola_nowa.sql">Baza danych</a></li>
-        </ul>
-
-        <hr class="horizontal-line">
-
-        <h3>Wyznaczenie najwyższej średniej</h3>
-        <ul>
-            <li>Polak: <input type="text" /></li>
-            <li>Nowak: <input type="text" /></li>
-            
-        </ul>
-
-        <button>Wyznacz</button>
-
-        <h3>Najwyższa średnia</h3>
+    <section class="boki">
+        <section class="lewy">
+            <h2>Zadanie 1 - przeglądanie zdjęć</h2>
+            <button>Poprzednie</button>
+                <img src="zdj1.jpg" alt="galeria" class="duze">
+            <button onclick="zdjecia()">Następne</button>
+            <hr>
+            <img src="zdj1.jpg" alt="miniatura" class="miniatura" onclick="zdjecia(1)">
+            <img src="zdj2.jpg" alt="miniatura" class="miniatura" onclick="zdjecia(2)">
+            <img src="zdj3.jpg" alt="miniatura" class="miniatura" onclick="zdjecia(3)"> 
+            <img src="zdj4.jpg" alt="miniatura" class="miniatura" onclick="zdjecia(4)">
+            <img src="zdj5.jpg" alt="miniatura" class="miniatura" onclick="zdjecia(5)">
+        </section>
+        <section class="prawy">
+            <h2>Podaj składową H koloru w kodzie HSL</h2>
+                <p>0 do 30 czerwienie, do 60 żółcie, do 150 zielenie, do 250 niebieskie, do 360 fiolet i purpura</p>
+                <img src="kolory.jpg" alt="Przestrzeń barw HSL"><br>
+                <input type="number" max="360" placeholder="0 - 360" id="hsl">
+                <input type="submit" value="Generuj paletę" onclick="guzik()">
+                <table id="tabela">
+                    <tr>
+                        <td id="1" colspan="4">Kolor główny</td>
+                    </tr>
+                    <tr>
+                        <td id="2">kolor 2</td>
+                        <td id="3">kolor 3</td>
+                        <td id="4">kolor 4</td>
+                        <td id="5">kolor 5</td>
+                    </tr>
+                </table>
+                <script>
+                    function guzik(){
+                        let wartosc=document.getElementById("hsl").value;
+                        const tabela=document.getElementById("tabela");
+                        tabela.rows[0].cells[0].style.background= 'hsl('+wartosc+',80%,50%)';
+                        tabela.rows[1].cells[0].style.background= 'hsl('+wartosc+',70%,50%)';
+                        tabela.rows[1].cells[1].style.background= 'hsl('+wartosc+',60%,50%)';
+                        tabela.rows[1].cells[2].style.background= 'hsl('+wartosc+',50%,50%)';
+                        tabela.rows[1].cells[3].style.background= 'hsl('+wartosc+',40%,50%)';
+                    }
+                </script>
+        </section>
     </section>
-
-    <section class="right-panel">
-        <table>
-            <tr>
-                <th>Treść zapytania</th>
-                <th>Zrzut ekranu</th>
-            </tr>
-            <tr>
-                <td>Treść zapytania 1</td>
-                <td><img src="zrzut1.jpg" alt="zrzut1.jpg" /></td>
-            </tr>
-            <tr>
-                <td>Treść zapytania 2</td>
-                <td><img src="zrzut2.jpg" alt="zrzut2.jpg" /></td>
-            </tr>
-            <tr>
-                <td>Treść zapytania 3</td>
-                <td><img src="zrzut3.jpg" alt="zrzut3.jpg" /></td>
-            </tr>
-            <tr>
-                <td>Treść zapytania 4</td>
-                <td><img src="zrzut4.jpg" alt="zrzut4.jpg" /></td>
-            </tr>
-            <tr>
-                <td>Treść zapytania 5</td>
-                <td><img src="zrzut5.jpg" alt="zrzut5.jpg" /></td>
-            </tr>
-        </table>
+    <section class="srodek">
+        <h2>Zadanie 3 - Obliczanie wartości dla danej osoby</h2>
+        <label for="wynik">Podaj wynik</label>
+        <input type="number" name="wynik">
+        <br>
+        <button>Oblicz</button>
+        <hr>
+        <output>
+            Wynik działania
+        </output>
     </section>
-
-    <section class="footer">
-        <h4>Stronę wykonał Sebastian<h4>
-    </section>
+    
+    <script>
+        function zdjecia(nr){
+             if(nr=="1"){
+                 document.getElementById("duzy").src="1.jpg"
+             }
+             if(nr=="2"){
+                 document.getElementById("duzy").src="2.jpg"
+             }
+             if(nr=="3"){
+                 document.getElementById("duzy").src="3.jpg"
+             }
+             if(nr=="4"){
+                 document.getElementById("duzy").src="4.jpg"
+             }
+             if(nr=="5"){
+                 document.getElementById("duzy").src="5.jpg"
+             }
+         } 
+         
+     </script>
+ 
+    <footer>
+        Stronę wykonał Przemek
+    </footer>
 </body>
 </html>
-
-    
-    
-    
-    
-   // Zmienne globalne
-var activePhoto = 1;
-
-// Funkcja obsługująca kliknięcie przycisku "Następne"
-function nextPhoto() {
-  activePhoto++;
-  if (activePhoto > 7) {
-    activePhoto = 1;
-  }
-  displayActivePhoto();
-}
-
-// Funkcja obsługująca kliknięcie przycisku "Poprzednie"
-function previousPhoto() {
-  activePhoto--;
-  if (activePhoto < 1) {
-    activePhoto = 7;
-  }
-  displayActivePhoto();
-}
-
-// Funkcja obsługująca kliknięcie miniatury zdjęcia
-function selectPhoto(photoNumber) {
-  activePhoto = photoNumber;
-  displayActivePhoto();
-}
-
-// Funkcja wyświetlająca aktywne zdjęcie
-function displayActivePhoto() {
-  var activePhotoElement = document.getElementById("active-photo");
-  activePhotoElement.src = "photos/photo" + activePhoto + ".jpg";
-}
-
-// Inicjalizacja skryptu po załadowaniu strony
-document.addEventListener("DOMContentLoaded", function(event) {
-  displayActivePhoto();
-
-  var nextButton = document.getElementById("next-button");
-  nextButton.addEventListener("click", nextPhoto);
-
-  var previousButton = document.getElementById("previous-button");
-  previousButton.addEventListener("click", previousPhoto);
-
-  var thumbnailElements = document.getElementsByClassName("thumbnail");
-  for (var i = 0; i < thumbnailElements.length; i++) {
-    thumbnailElements[i].addEventListener("click", function() {
-      var photoNumber = parseInt(this.getAttribute("data-photo-number"));
-      selectPhoto(photoNumber);
-    });
-  }
-});
- 
